428fd75e98598243b26b20bec2b43bc3e8178185
Rudy Jessop <rudyjessop@gmail.com>
2015-09-28 11:09:37 -0400

    Creating an UsersPhoto model

---
+++ app/UsersPhotos.php
@@ -0,0 +1,49 @@
+<?php namespace App;
+
+use Storage;
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File;
+
+
+class UsersPhotos extends Model {
+
+  /**
+   * Declearing usersImages
+   * @var string
+   */
+  protected $table = 'usersImages';
+
+  /**
+   * Declearing Fillable columns
+   * @var array
+   */
+  protected $fillable = ['image_path', 'image_name', 'image_thumbnail'];
+
+  protected $dates = ['timestamps'];
+
+  /**
+   * Relationship to the User Model
+   * @return App\User
+   */
+  public function user(){
+    return $this->belongsTo('App\User');
+  }
+
+  public static function requestImage(UploadedFile $file){
+
+    $photo = new static;
+
+    $Imagename = time().$file->getClientOriginalName(); // Gives the file its name
+
+    $userfilepath = $UserData->getUsername(); // Get current username to define directory
+
+    $holder = Storage::disk('userPhotos')->put($userfilepath.'/'.$Imagename, '');// Prep Image for disk on folder
+
+    return $photo;
+
+  }
+
+
+
+
+}
