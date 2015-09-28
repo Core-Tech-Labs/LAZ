5bc39a6fc713b7fcd3aa4d7e5932ac18eacff383
Rudy Jessop <rudyjessop@gmail.com>
2015-09-28 11:01:30 -0400

    changing css file name

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -24,24 +24,17 @@
 *   (Needs to move to custom.js)
 */
 
-    $(document).on('change', '.btn-file :file', function() {
-  var input = $(this),
-      numFiles = input.get(0).files ? input.get(0).files.length : 1,
-      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-  input.trigger('fileselect', [numFiles, label]);
-});
 
-$(document).ready( function() {
-    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
 
-        var input = $(this).parents('.input-group').find(':text'),
-            log = numFiles > 1 ? numFiles + ' files selected' : label;
+/**
+ * Dropzone
+ *
+ */
 
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
-        }
+Dropzone.options.massUpload = {
+    maxFilesize: 3,
+    acceptedFiles: '.jpg, .jpeg, .png',
+    clickable: true,
 
-    });
-});
+};
+
