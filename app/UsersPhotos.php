a7dcdef7d2c05af3f2526a4b850a8b58f5bc66c5
Rudy Jessop <rudyjessop@gmail.com>
2015-11-04 12:14:13 -0500

    Refactoring business logic for image controllers

--- app/UsersPhotos.php
+++ app/UsersPhotos.php
@@ -1,9 +1,13 @@
 <?php
 namespace App;
 
-use Storage;
+use Request;
+use App\User;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\HttpFoundation\File;
+use Illuminate\Contracts\Filesystem\Factory as Filesystem;
+
 
 
 class UsersPhotos extends Model {
@@ -30,21 +34,53 @@ class UsersPhotos extends Model {
     return $this->belongsTo('App\User');
   }
 
-  public static function requestImage(UploadedFile $file){
+  /**
+   * Business logic for saving images
+   * @param Request    $request    [description]
+   * @param User       $user       [description]
+   * @param Filesystem $filesystem [description]
+   */
+  public function UsersUploadedImages(Request $request, User $user, Filesystem $filesystem){
+    // Photo(s) Validation
+      $this->Validate($request,[
+          'file' => 'required|max:3000|mimes:jpg,jpeg,png',
+      ]);
 
-    $photo = new static;
+      $file = $request->file('file');
+      $Imagename = time().$file->getClientOriginalName();
 
-    $Imagename = time().$file->getClientOriginalName(); // Gives the file its name
-
-    $userfilepath = $UserData->getUsername(); // Get current username to define directory
-
-    $holder = Storage::disk('userPhotos')->put($userfilepath.'/'.$Imagename, '');// Prep Image for disk on folder
-
-    return $photo;
+      $ProfilePicturePath = $filesystem->disk('s3')->put($user->username.'/'.$Imagename, '');
+      //Create New Image Upload Instance to database
+      $user->usersPhotos()->create([
+              'image_path' => $ProfilePicturePath,
+              'image_name' => $Imagename,
+      ]);
 
   }
 
 
+  /**
+   * Business logic for saving users Profile Picture
+   * @param  Request $request [description]
+   * @return [type]           [description]
+   */
+  public function UserProfilePicture(Request $request, User $user, Filesystem $filesystem){
+      $this->Validate($request,[
+          'file' => 'required|max:3000|mimes:jpg,jpeg,png',
+      ]);
+
+      $file = $request->file('file');
+      $Imagename = .'DP'.time().$file->$user->username;
+      $ProfilePicturePath = $filesystem->disk('s3')->put($user->username.'/'.$Imagename, '');
+
+      //Create New Image Upload Instance to database
+      $user->userData()->create([
+              'profile_picture' => $ProfilePicturePath,
+              'picture_name' => $Imagename
+
+      ]);
+
+  }
 
 
 }
