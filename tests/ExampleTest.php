a5ca82196bffb1f719dfb6faefc3a627f8c15736
Rudy Jessop <rudyjessop@gmail.com>
2015-11-11 11:26:40 -0500

    start testing

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,4 +14,32 @@ class ExampleTest extends TestCase {
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
+	public function welcomeAndLoginUser(){
+		$this->visits('/')
+				 ->see('LAZ')
+				 ->see('Log In')
+				 ->click('/login')
+				 ->seePageIs('/login')
+				 ->type('email@email.com', 'email')
+				 ->type('password', 'password')
+				 ->press('login')
+				 ->seePageIs('/home');
+	}
+
+	public function welcomeAndRegisterUser(){
+		$this->visits('/')
+				 ->see('LAZ')
+				 ->see('Register')
+				 ->click('/register')
+				 ->seePageIs('/register')
+				 ->type('09/09/1920','_dob')
+				 ->type('00000', 'zip')
+				 ->type('username', 'username')
+				 ->type('email@email.com', 'email')
+				 ->type('password', 'password')
+				 ->type('password', 'password_confirmation')
+				 ->press('register')
+				 ->seePageIs('/dashboard');
+	}
+
 }
