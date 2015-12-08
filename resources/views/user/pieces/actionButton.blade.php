72114f41f4344a821bdfd7cc2dc0f51ea329e387
Rudy Jessop <rudyjessop@gmail.com>
2015-12-08 12:20:57 -0500

    clean up

--- resources/views/user/pieces/actionButton.blade.php
+++ resources/views/user/pieces/actionButton.blade.php
@@ -4,8 +4,22 @@
   {!! Form::open(['action'=>'FavController@store']) !!}
     {!! Form::hidden('userIDToFav', $UserData->id) !!}
   <div class="btn-group">
-      <button type="submit" class="btn btn-default">Add Fav</button>
- {{-- <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <button type="submit" class="btn btn-default">Add Fav</button>
+    {!! Form::close() !!}
+    {{-- @else --}}
+    <!-- User Favorited form
+    <div>You are following {{ $UserData->username }}</div> -->
+    {{-- @endif --}}
+    <button type="button" class="btn btn-default">Message</button>
+  </div>
+  @else
+    <a type="" class="btn btn-default">View My Activity</a>
+  @endif
+
+
+  {{-- UNUSED CODE --}}
+
+   {{-- <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <span class="caret"></span>
       <span class="sr-only">Toggle Dropdown</span>
     </button> --}}
@@ -16,14 +30,3 @@
       <li role="separator" class="divider"></li>
       <li><a href="#">Separated link</a></li>
     </ul> --}}
-
-  {!! Form::close() !!}
-  {{-- @else --}}
-    <!-- User Favorited form
-    <div>You are following {{ $UserData->username }}</div> -->
-  {{-- @endif --}}
-<button type="button" class="btn btn-default">Message</button>
-</div>
-  @else
-    <a type="" class="btn btn-default">View My Activity</a>
-  @endif
