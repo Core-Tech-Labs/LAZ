68bebb5eec56cfbc7ef6e105b964e20f483a637b
Rudy Jessop <rudyjessop@gmail.com>
2015-12-09 17:54:27 -0500

    fixing mysql local issue

--- config/database.php
+++ config/database.php
@@ -59,6 +59,7 @@ return [
 			'database'  => env('DB_DATABASE'),
 			'username'  => env('DB_USERNAME'),
 			'password'  => env('DB_PASSWORD'),
+			'unix_socket'=> env('DB_SOCKET'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
