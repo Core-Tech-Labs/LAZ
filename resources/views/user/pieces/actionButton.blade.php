a02c4535ec969fcc90784f1f38b93fc229709487
Rudy Jessop <rudyjessop@gmail.com>
2015-12-14 18:39:54 -0500

    revert modz

--- resources/views/user/pieces/actionButton.blade.php
+++ resources/views/user/pieces/actionButton.blade.php
@@ -1,38 +1,24 @@
 @if (Auth::user()->username !== $UserData->username  )
-
-    @if (!$UserData->checkFavorited() )
-    <!-- Checks if Auth user is favorited -->
-    <div class="btn-group">
-      {!! Form::open(['action'=>'FavController@store']) !!}
-      {!! Form::hidden('userIDToFav', $UserData->id) !!}
-
-        <button type="submit" class="btn btn-success">Add Fav</button>
-      {!! Form::close() !!}
-    @else
-    <!-- Focus and unfavorite user-->
-    {!! Form::open(['method' => 'DELETE', 'action' => ['FavController@destroy', $UserData->id] ]) !!}
-      {!! Form::hidden('userIDToUnFav', $UserData->id) !!}
-      <div class="btn-group">
-        <button type="submit" class="btn btn-primary">Favorited</button>
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="caret"></span>
-        <span class="sr-only">Toggle Dropdown</span>
-        </button>
-      <ul class="dropdown-menu">
-        <li><a href="#">View {{ $UserData->username }}'s Activity</a></li>
-      </ul>
-    </div>
+  {{-- @if ($UserData->isFavedBy() ) --}}
+  <!-- User Not Favorited form -->
+  {!! Form::open(['action'=>'FavController@store']) !!}
+    {!! Form::hidden('userIDToFav', $UserData->id) !!}
+  <div class="btn-group">
+    <button type="submit" class="btn btn-default">
+          Add Fav
+    </button>
     {!! Form::close() !!}
-    @endif
-        <button type="button" class="btn btn-default">Message</button>
+    {{-- @else --}}
+    <!-- User Favorited form
+    <div>You are following {{ $UserData->username }}</div> -->
+    {{-- @endif --}}
+    <button type="button" class="btn btn-default">Message</button>
   </div>
-
-
   @else
     <a type="" class="btn btn-default">View My Activity</a>
-@endif
+  @endif
 
-<!--
+
   {{-- UNUSED CODE --}}
 
    {{-- <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -45,4 +31,4 @@
       <li><a href="#">-</a></li>
       <li role="separator" class="divider"></li>
       <li><a href="#">Separated link</a></li>
-    </ul>-->
+    </ul> --}}
