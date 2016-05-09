4a50377dc1ae2872b830e89c8cf8d2a3f21199d9
Rudy Jessop <rudyjessop@gmail.com>
2016-05-09 17:21:38 -0400

    adding finishing touches to flash messages

--- resources/views/flash-msg/flash.blade.php
+++ resources/views/flash-msg/flash.blade.php
@@ -5,16 +5,19 @@
 </script>
 @if (Session('success_message'))
             <div id="success" class="alert alert-success {{ Session::has('flash_message_important')? 'alert-important' : '' }}{{-- Example line--}}">
+            <span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span>
                 @if(Session::has('flash_important')) {{-- Example line--}}
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 @endif
                 {{ Session::get('success_message') }} {{-- Example line session('success_message') --}}
             </div>
 @endif
-        
+
 
 @if (Session('failure_message'))
-        <div id="failure" class="alert alert-success {{ Session::has('flash_message_important')? 'alert-important' : '' }}">
-                {{ Session::get('success_message') }} 
+        <div id="failure" class="alert alert-danger {{ Session::has('flash_message_important')? 'alert-important' : '' }}">
+        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                {{ Session::get('failure_message') }}
+                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         </div>
 @endif
