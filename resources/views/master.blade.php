0b6a673c2a52e0cf82e0d926e134856ace2f95db
Rudy Jessop <rudyjessop@gmail.com>
2016-11-30 12:42:49 -0500

    editz

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>LAZ | @yield('title')</title>
+  {{-- Branch laz-msg --}}
 
 
 	      <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
